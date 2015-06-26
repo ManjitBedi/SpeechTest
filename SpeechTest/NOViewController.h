7ca11deb49784121856b84f80c237e7b5614a1a6
ManjitBedi <work@manjitbedi.com>
2015-06-25 21:21:24 -0700

    adjusted the UI basic code is in place

--- SpeechTest/NOViewController.h
+++ SpeechTest/NOViewController.h
@@ -6,8 +6,30 @@
 //  Copyright (c) 2014 noorg. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+// press Home button,
+// go back to app, switch off the screen,
+// switch on,
+// home,
+// switch off,
+// switch on,
+// back to app etc etc,
+// you will finally get the problem. No sound at all! (It seems that the sequence to reproduce it, is different all the time, but pressing Home, switch off at random will finally manifest the problem after 2-3 minutes on my iPhone.)
 
-@interface NOViewController : UIViewController
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface NOViewController : UIViewController <AVSpeechSynthesizerDelegate> {
+    AVSpeechSynthesizer* _synthesizer;
+    NSTimer* _timer;
+    BOOL _repeatTimer;
+}
+@property (weak, nonatomic) IBOutlet UILabel *speechStatusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *avSessionStatus;
+- (IBAction)playJackHandy:(id)sender;
+- (IBAction)playHamlet:(id)sender;
+- (IBAction)playJackAfterDelay:(id)sender;
+- (IBAction)playHamletAfterDelay:(id)sender;
+- (IBAction)repeatTimerSwitchValueChanged:(id)sender;
+
 
 @end
--- SpeechTest/NOViewController.m
+++ SpeechTest/NOViewController.m
@@ -6,6 +6,47 @@
 //  Copyright (c) 2014 noorg. All rights reserved.
 //
 
+NSString *hamlet =
+
+@"To be, or not to be--that is the question:"
+"Whether 'tis nobler in the mind to suffer"
+"The slings and arrows of outrageous fortune"
+"Or to take arms against a sea of troubles"
+"And by opposing end them. To die, to sleep--"
+"No more--and by a sleep to say we end"
+"The heartache, and the thousand natural shocks"
+"That flesh is heir to. 'Tis a consummation"
+"Devoutly to be wished. To die, to sleep--"
+"To sleep--perchance to dream: ay, there's the rub,"
+"For in that sleep of death what dreams may come"
+"When we have shuffled off this mortal coil,"
+"Must give us pause. There's the respect"
+"That makes calamity of so long life."
+"For who would bear the whips and scorns of time,"
+"Th' oppressor's wrong, the proud man's contumely"
+"The pangs of despised love, the law's delay,"
+"The insolence of office, and the spurns"
+"That patient merit of th' unworthy takes,"
+"When he himself might his quietus make"
+"With a bare bodkin? Who would fardels bear,"
+"To grunt and sweat under a weary life,"
+"But that the dread of something after death,"
+"The undiscovered country, from whose bourn"
+"No traveller returns, puzzles the will,"
+"And makes us rather bear those ills we have"
+"Than fly to others that we know not of?"
+"Thus conscience does make cowards of us all,"
+"And thus the native hue of resolution"
+"Is sicklied o'er with the pale cast of thought,"
+"And enterprise of great pitch and moment"
+"With this regard their currents turn awry"
+"And lose the name of action. -- Soft you now,"
+"The fair Ophelia! -- Nymph, in thy orisons"
+"Be all my sins remembered.";
+
+NSString *jackHandy = @"Why do people in ship mutinies always ask for \"better treatment\"? I'd ask for a pinball machine, because with all that rocking back and forth you'd probably be able to get a lot of free games.";
+
+
 #import "NOViewController.h"
 
 @interface NOViewController ()
@@ -14,16 +55,118 @@
 
 @implementation NOViewController
 
-- (void)viewDidLoad
-{
+
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+//    NSError *error;
+//    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback withOptions:AVAudioSessionCategoryOptionDuckOthers error:&error];
+//    if(error){
+//        _avSessionStatus.text = [error localizedDescription];
+//    }
+//
+//    [[AVAudioSession sharedInstance] setActive:YES error:&error];
+//    if(error){
+//        _avSessionStatus.text = [error localizedDescription];
+//    }
+    
+    _synthesizer = [[AVSpeechSynthesizer alloc] init];
+    _synthesizer.delegate = self;
+    
+    _repeatTimer = YES;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appWillEnterForeground:) name:UIApplicationWillEnterForegroundNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
 }
 
-- (void)didReceiveMemoryWarning
-{
+
+- (void)viewDidUnload {
+    if(_timer)
+        [_timer invalidate];
+    
+    [super viewDidUnload];
+}
+
+
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)playJackHandy:(id)sender {
+    [self speak:jackHandy];
+}
+
+
+- (IBAction)playHamlet:(id)sender {
+    [self speak:hamlet];
+}
+
+
+- (IBAction)playJackAfterDelay:(id)sender {
+    NSLog(@"star timer");
+    NSDictionary *userInfo = @{ @"speech" : jackHandy };
+    _timer = [NSTimer timerWithTimeInterval:4 target:self selector:@selector(progressTimerFired:) userInfo:userInfo repeats:_repeatTimer];
+    [[NSRunLoop currentRunLoop]addTimer:_timer forMode:NSDefaultRunLoopMode];
+}
+
+
+- (IBAction)playHamletAfterDelay:(id)sender {
+    NSLog(@"star timer");
+    NSDictionary *userInfo = @{ @"speech" : hamlet };
+    _timer = [NSTimer timerWithTimeInterval:4 target:self selector:@selector(progressTimerFired:) userInfo:userInfo repeats:_repeatTimer];
+    [[NSRunLoop currentRunLoop]addTimer:_timer forMode:NSDefaultRunLoopMode];
+}
+
+- (IBAction)repeatTimerSwitchValueChanged:(id)sender {
+    _repeatTimer = (UISwitch *) sender;
+}
+
+#pragma mark -
+- (void)appWillEnterForeground:(NSNotification*)aNotification {
+    NSLog (@"app will enter foreground");
+}
+
+
+- (void)appDidEnterBackground:(NSNotification*)aNotification {
+    NSLog (@"app did enter background");
+}
+
+
+#pragma mark -
+- (void) speak:(NSString *) speechString {
+    _speechStatusLabel.text = @"start speech";
+    AVSpeechUtterance *utterance1 = [[AVSpeechUtterance alloc] initWithString:speechString];
+    [self initUtterance:utterance1];
+    [_synthesizer speakUtterance:utterance1];
+}
+
+
+- (void) progressTimerFired:(NSTimer *)timer {
+    NSLog(@"timer event fired");
+    NSDictionary *userInfo = timer.userInfo;
+    NSString *speechString = userInfo[@"speech"];
+    [self speak:speechString];
+}
+
+
+- (void)initUtterance:(AVSpeechUtterance*)utterance {
+    AVSpeechSynthesisVoice* voice = [AVSpeechSynthesisVoice voiceWithLanguage:@"en-US"];
+    utterance.rate = 0.1f;
+    utterance.volume = 1.0;
+    utterance.voice = voice;
+}
+
+
+- (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didStartSpeechUtterance:(AVSpeechUtterance *)utterance {
+    NSLog(@"start speech utterance");
+}
+
+
+- (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didFinishSpeechUtterance:(AVSpeechUtterance *)utterance {
+    NSLog(@"speech utterance finished");
+    _speechStatusLabel.text = @"speech utterance finished";
+}
+
 
 @end
